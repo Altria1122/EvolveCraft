8a632fccef2cc7998a5d65acd7c82022863195c3
LiuTao <44425349+Altria1122@users.noreply.github.com>
2025-07-02 12:14:03 +0800

    Initial commit

---
+++ evolveCraft-admin/src/main/java/com/altria/evolvecraftadmin/EvolveCraftAdminApplication.java
@@ -0,0 +1,13 @@
+package com.altria.evolvecraftadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolveCraftAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvolveCraftAdminApplication.class, args);
+    }
+
+}
---
+++ evolveCraft-admin/src/test/java/com/altria/evolvecraftadmin/EvolveCraftAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altria.evolvecraftadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolveCraftAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
